260ada534989ca0db8f713fa6e8fb20fd5c845bf
Mohamed Khaled <gemyfolks1@gmail.com>
2024-01-06 07:11:07 +0200

    fix: Use capture website instead of playwright or puppeteer

--- app/api/screenshot/route.ts
+++ app/api/screenshot/route.ts
@@ -1,9 +1,13 @@
-import {
-  getScreenshotPlaywright,
-  getScreenshotPuppeteer,
-} from "lib/getScreenshot";
+import captureWebsite from "capture-website";
 import { NextRequest, NextResponse } from "next/server";
+
+const url = "https://portfolio.mohamed-khaled.com/";
 export async function GET(req: NextRequest, res: NextResponse) {
-  const screenshot = await getScreenshotPlaywright();
-  return new Response(screenshot);
+  const screenshot = await captureWebsite.base64(url, {
+    darkMode: true,
+    width: 1100,
+    height: 800,
+  });
+  const prefix = "data:image/png;base64,";
+  return new Response(`${prefix}${screenshot}`);
 }
--- lib/getScreenshot.ts
+++ lib/getScreenshot.ts
@@ -1,44 +1,40 @@
-import puppeteer from "puppeteer";
+// const url = "https://portfolio.mohamed-khaled.com/";
 
-import { chromium } from "playwright";
+// export async function getScreenshotPlaywright(): Promise<string> {
+//   const browser = await chromium.launch({ args: ["--no-sandbox"] });
+//   const page = await browser.newPage();
+//   await page.setViewportSize({ width: 1100, height: 800 });
+//   await page.goto(url, {
+//     waitUntil: "domcontentloaded",
+//   });
+//   await new Promise((resolve) => setTimeout(resolve, 500));
+//   const screenshot = await page.screenshot();
+//   const screenshotBase64 = screenshot.toString("base64");
+//   await browser.close();
 
-const url = "https://portfolio.mohamed-khaled.com/";
+//   const prefix = "data:image/png;base64,";
+//   return `${prefix}${screenshotBase64}`;
+//   // return screenshotBase64;
+// }
 
-export async function getScreenshotPlaywright(): Promise<string> {
-  const browser = await chromium.launch({ args: ["--no-sandbox"] });
-  const page = await browser.newPage();
-  await page.setViewportSize({ width: 1100, height: 800 });
-  await page.goto(url, {
-    waitUntil: "domcontentloaded",
-  });
-  await new Promise((resolve) => setTimeout(resolve, 500));
-  const screenshot = await page.screenshot();
-  const screenshotBase64 = screenshot.toString("base64");
-  await browser.close();
+// export async function getScreenshotPuppeteer(): Promise<string> {
+//   const browser = await puppeteer.launch({
+//     headless: "new",
+//     defaultViewport: {
+//       width: 1100,
+//       height: 800,
+//     },
+//     args: ["--no-sandbox"],
+//   });
+//   const page = await browser.newPage();
+//   await page.goto(url, {
+//     waitUntil: "domcontentloaded",
+//   });
+//   await new Promise((resolve) => setTimeout(resolve, 500));
+//   const screenshot = await page.screenshot();
+//   const screenshotBase64 = screenshot.toString("base64");
+//   await browser.close();
 
-  const prefix = "data:image/png;base64,";
-  return `${prefix}${screenshotBase64}`;
-  // return screenshotBase64;
-}
-
-export async function getScreenshotPuppeteer(): Promise<string> {
-  const browser = await puppeteer.launch({
-    headless: "new",
-    defaultViewport: {
-      width: 1100,
-      height: 800,
-    },
-    args: ["--no-sandbox"],
-  });
-  const page = await browser.newPage();
-  await page.goto(url, {
-    waitUntil: "domcontentloaded",
-  });
-  await new Promise((resolve) => setTimeout(resolve, 500));
-  const screenshot = await page.screenshot();
-  const screenshotBase64 = screenshot.toString("base64");
-  await browser.close();
-
-  const prefix = "data:image/png;base64,";
-  return `${prefix}${screenshotBase64}`;
-}
+//   const prefix = "data:image/png;base64,";
+//   return `${prefix}${screenshotBase64}`;
+// }
