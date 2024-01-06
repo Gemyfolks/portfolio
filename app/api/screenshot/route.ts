ae4b8895a70d1259f28af59e60e3630fbd09d1c6
Mohamed Khaled <gemyfolks1@gmail.com>
2024-01-06 06:45:34 +0200

    fix: Use Puppeteer instead of Playwright

--- app/api/screenshot/route.ts
+++ app/api/screenshot/route.ts
@@ -3,8 +3,7 @@ import {
   getScreenshotPuppeteer,
 } from "lib/getScreenshot";
 import { NextRequest, NextResponse } from "next/server";
-
 export async function GET(req: NextRequest, res: NextResponse) {
-  const screenshot = await getScreenshotPlaywright();
+  const screenshot = await getScreenshotPuppeteer();
   return new Response(screenshot);
 }
