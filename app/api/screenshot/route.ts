90bc870b4087b21f7a0a7d1b557ae0f8f3278e13
Mohamed Khaled <gemyfolks1@gmail.com>
2024-01-06 07:24:07 +0200

    use puppeteer instead of capture websites with no sand box args

--- app/api/screenshot/route.ts
+++ app/api/screenshot/route.ts
@@ -1,13 +1,6 @@
-import captureWebsite from "capture-website";
+import { getScreenshotPuppeteer } from "lib/getScreenshot";
 import { NextRequest, NextResponse } from "next/server";
-
-const url = "https://portfolio.mohamed-khaled.com/";
 export async function GET(req: NextRequest, res: NextResponse) {
-  const screenshot = await captureWebsite.base64(url, {
-    darkMode: true,
-    width: 1100,
-    height: 800,
-  });
-  const prefix = "data:image/png;base64,";
-  return new Response(`${prefix}${screenshot}`);
+  const screenshot = await getScreenshotPuppeteer();
+  return new Response(screenshot);
 }
--- lib/getScreenshot.ts
+++ lib/getScreenshot.ts
@@ -1,4 +1,6 @@
-// const url = "https://portfolio.mohamed-khaled.com/";
+import puppeteer from "puppeteer";
+
+const url = "https://portfolio.mohamed-khaled.com/";
 
 // export async function getScreenshotPlaywright(): Promise<string> {
 //   const browser = await chromium.launch({ args: ["--no-sandbox"] });
@@ -17,24 +19,24 @@
 //   // return screenshotBase64;
 // }
 
-// export async function getScreenshotPuppeteer(): Promise<string> {
-//   const browser = await puppeteer.launch({
-//     headless: "new",
-//     defaultViewport: {
-//       width: 1100,
-//       height: 800,
-//     },
-//     args: ["--no-sandbox"],
-//   });
-//   const page = await browser.newPage();
-//   await page.goto(url, {
-//     waitUntil: "domcontentloaded",
-//   });
-//   await new Promise((resolve) => setTimeout(resolve, 500));
-//   const screenshot = await page.screenshot();
-//   const screenshotBase64 = screenshot.toString("base64");
-//   await browser.close();
+export async function getScreenshotPuppeteer(): Promise<string> {
+  const browser = await puppeteer.launch({
+    headless: "new",
+    defaultViewport: {
+      width: 1100,
+      height: 800,
+    },
+    args: ["--no-sandbox"],
+  });
+  const page = await browser.newPage();
+  await page.goto(url, {
+    waitUntil: "domcontentloaded",
+  });
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  const screenshot = await page.screenshot();
+  const screenshotBase64 = screenshot.toString("base64");
+  await browser.close();
 
-//   const prefix = "data:image/png;base64,";
-//   return `${prefix}${screenshotBase64}`;
-// }
+  const prefix = "data:image/png;base64,";
+  return `${prefix}${screenshotBase64}`;
+}
