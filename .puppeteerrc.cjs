156766587459e1aafcf5c09e7dcf5d384b5e99ea
Mohamed Khaled <gemyfolks1@gmail.com>
2024-01-12 10:16:04 +0200

    Update .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,5 +5,13 @@ const { join } = require("path");
  */
 module.exports = {
   // Changes the cache location for Puppeteer .
-  cacheDirectory: join(__dirname, ".cache", "puppeteer"),
+  cacheDirectory: join(
+    __dirname,
+    ".cache",
+    "puppeteer",
+    "chrome",
+    "linux-119.0.6045.105",
+    "chrome-linux64",
+    "chrome"
+  ),
 };
