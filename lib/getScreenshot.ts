c38421ada62d3d06fccd2443d837e0b7ec3bec05
Mohamed Khaled <gemyfolks1@gmail.com>
2024-01-12 09:37:32 +0200

    Update getScreenshot.ts

--- lib/getScreenshot.ts
+++ lib/getScreenshot.ts
@@ -2,30 +2,14 @@ import puppeteer from "puppeteer";
 
 const url = "https://portfolio.mohamed-khaled.com/";
 
-// export async function getScreenshotPlaywright(): Promise<string> {
-//   const browser = await chromium.launch({ args: ["--no-sandbox"] });
-//   const page = await browser.newPage();
-//   await page.setViewportSize({ width: 1100, height: 800 });
-//   await page.goto(url, {
-//     waitUntil: "domcontentloaded",
-//   });
-//   await new Promise((resolve) => setTimeout(resolve, 500));
-//   const screenshot = await page.screenshot();
-//   const screenshotBase64 = screenshot.toString("base64");
-//   await browser.close();
-
-//   const prefix = "data:image/png;base64,";
-//   return `${prefix}${screenshotBase64}`;
-//   // return screenshotBase64;
-// }
-
 export async function getScreenshotPuppeteer(theme: string): Promise<string> {
   const browser = await puppeteer.launch({
-    headless: "new",
+    headless: true,
     defaultViewport: {
       width: 1100,
       height: 800,
     },
+
     args: ["--no-sandbox"],
   });
   const page = await browser.newPage();
